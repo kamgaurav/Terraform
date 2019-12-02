f9c5613f72210c151e53032e4d636ae3aa5226cb
kamgaurav <kamgaurav83@gmail.com>
2019-12-02 20:19:20 +0530

    Chapter 3

--- modules_example/stage/data-stores/mysql/secret.tfvars
+++ modules_example/stage/data-stores/mysql/secret.tfvars
@@ -2,3 +2,5 @@ aws_access_key = ""
 aws_secret_key = ""
 
 region = "us-east-1"
+
+db_password = ""
--- modules_example/stage/services/webserver-cluster/var.tf
+++ modules_example/stage/services/webserver-cluster/var.tf
@@ -1,5 +1,8 @@
 variable "aws_access_key" {}
 variable "aws_secret_key" {}
+
+variable "region" {}
+
 variable "server_port" {
   description = "The HTTP port number"
   default     = 8080
